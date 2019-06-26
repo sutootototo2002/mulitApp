58898407264b79e5a04b003b54f095db83d88348
sutootototo2002 <sutootototo2002@163.com>
2019-06-26 16:59:59 +0800

    支付分 改为开发区

--- dist_payfen_weapp/npm/@tarojs/taro-weapp/index.js
+++ dist_payfen_weapp/npm/@tarojs/taro-weapp/index.js
@@ -1,1 +1,2 @@
-module.exports=require("./dist/index.js").default,module.exports.default=module.exports;
+module.exports = require('./dist/index.js').default;
+module.exports.default = module.exports;
--- dist_payfen_weapp/pages/service/service.js
+++ dist_payfen_weapp/pages/service/service.js
@@ -1,1 +1,143 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var _class,_temp2,_createClass=function(){function r(e,t){for(var n=0;n<t.length;n++){var r=t[n];r.enumerable=r.enumerable||!1,r.configurable=!0,"value"in r&&(r.writable=!0),Object.defineProperty(e,r.key,r)}}return function(e,t,n){return t&&r(e.prototype,t),n&&r(e,n),e}}(),_get=function e(t,n,r){null===t&&(t=Function.prototype);var o=Object.getOwnPropertyDescriptor(t,n);if(void 0===o){var i=Object.getPrototypeOf(t);return null===i?void 0:e(i,n,r)}if("value"in o)return o.value;var a=o.get;return void 0!==a?a.call(r):void 0},_index=require("../../npm/@tarojs/taro-weapp/index.js"),_index2=_interopRequireDefault(_index),_index3=require("../../config/index.js");function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}function _classCallCheck(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}function _possibleConstructorReturn(e,t){if(!e)throw new ReferenceError("this hasn't been initialised - super() hasn't been called");return!t||"object"!=typeof t&&"function"!=typeof t?e:t}function _inherits(e,t){if("function"!=typeof t&&null!==t)throw new TypeError("Super expression must either be null or a function, not "+typeof t);e.prototype=Object.create(t&&t.prototype,{constructor:{value:e,enumerable:!1,writable:!0,configurable:!0}}),t&&(Object.setPrototypeOf?Object.setPrototypeOf(e,t):e.__proto__=t)}var Login=(_temp2=_class=function(e){function a(){var e,t,n;_classCallCheck(this,a);for(var r=arguments.length,o=Array(r),i=0;i<r;i++)o[i]=arguments[i];return(t=n=_possibleConstructorReturn(this,(e=a.__proto__||Object.getPrototypeOf(a)).call.apply(e,[this].concat(o)))).$usedState=["loopArray0","loginImg","serverImg1","serverImg2","serverImg3","serverImg4","id_","server"],n.config={navigationBarTitleText:"联系客服"},n.$$refs=[],_possibleConstructorReturn(n,t)}return _inherits(a,_index.Component),_createClass(a,[{key:"_constructor",value:function(e){_get(a.prototype.__proto__||Object.getPrototypeOf(a.prototype),"_constructor",this).call(this,e),this.state={loginImg:_index3.PATH+"/mImages/loginImg.png",serverImg1:_index3.PATH+"/mImages/lxkf-1.png",serverImg2:_index3.PATH+"/mImages/lxkf-2.png",serverImg3:_index3.PATH+"/mImages/lxkf-3.png",serverImg4:_index3.PATH+"/mImages/lxkf-4.png",id_:0,server:[{id:0,img:_index3.PATH+"/mImages/lxkf-1.png",name:"订单计算错误"},{id:1,img:_index3.PATH+"/mImages/lxkf-2.png",name:"账号储值问题"},{id:2,img:_index3.PATH+"/mImages/lxkf-3.png",name:"货柜门打不开"},{id:3,img:_index3.PATH+"/mImages/lxkf-4.png",name:"货柜门无法锁上"}]}}},{key:"componentWillMount",value:function(){}},{key:"componentDidMount",value:function(){}},{key:"componentDidShow",value:function(){}},{key:"componentDidHide",value:function(){}},{key:"componentDidCatchError",value:function(){}},{key:"turnFn",value:function(e){console.log(e.currentTarget.dataset.id);var t=e.currentTarget.dataset.id;this.setState({id_:t})}},{key:"gohome",value:function(){_index2.default.navigateBack({})}},{key:"_createData",value:function(){var n=this;this.__state=arguments[0]||this.state||{},this.__props=arguments[1]||this.props||{};arguments[2];var e=this.__state.server.map(function(e,t){return e={$original:(0,_index.internal_get_original)(e)},{$loopState__temp2:n.__state.id_==Number(e.$original.id)?"box select":"box",$original:e.$original}});return Object.assign(this.__state,{loopArray0:e}),this.__state}}]),a}(),_class.properties={},_class.$$events=["turnFn","gohome"],_temp2);exports.default=Login,Component(require("../../npm/@tarojs/taro-weapp/index.js").default.createComponent(Login,!0));
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
+var _class, _temp2;
+
+var _index = require("../../npm/@tarojs/taro-weapp/index.js");
+
+var _index2 = _interopRequireDefault(_index);
+
+var _index3 = require("../../config/index.js");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+// 如果需要在 h5 环境中开启 React Devtools
+// 取消以下注释：
+// if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
+//   require('nerv-devtools')
+// }
+var Login = (_temp2 = _class = function (_BaseComponent) {
+  _inherits(Login, _BaseComponent);
+
+  function Login() {
+    var _ref;
+
+    var _temp, _this, _ret;
+
+    _classCallCheck(this, Login);
+
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Login.__proto__ || Object.getPrototypeOf(Login)).call.apply(_ref, [this].concat(args))), _this), _this.$usedState = ["loopArray0", "loginImg", "serverImg1", "serverImg2", "serverImg3", "serverImg4", "id_", "server"], _this.config = {
+      navigationBarTitleText: '联系客服'
+    }, _this.$$refs = [], _temp), _possibleConstructorReturn(_this, _ret);
+  }
+
+  _createClass(Login, [{
+    key: "_constructor",
+    value: function _constructor(props) {
+      _get(Login.prototype.__proto__ || Object.getPrototypeOf(Login.prototype), "_constructor", this).call(this, props);
+      /**
+       * 指定config的类型声明为: Taro.Config
+       *
+       * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+       * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+       * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+       */
+
+      this.state = {
+        loginImg: _index3.PATH + '/mImages/loginImg.png',
+        serverImg1: _index3.PATH + '/mImages/lxkf-1.png',
+        serverImg2: _index3.PATH + '/mImages/lxkf-2.png',
+        serverImg3: _index3.PATH + '/mImages/lxkf-3.png',
+        serverImg4: _index3.PATH + '/mImages/lxkf-4.png',
+        id_: 0,
+        server: [{ id: 0, img: _index3.PATH + '/mImages/lxkf-1.png', name: '订单计算错误' }, { id: 1, img: _index3.PATH + '/mImages/lxkf-2.png', name: '账号储值问题' }, { id: 2, img: _index3.PATH + '/mImages/lxkf-3.png', name: '货柜门打不开' }, { id: 3, img: _index3.PATH + '/mImages/lxkf-4.png', name: '货柜门无法锁上' }]
+      };
+    }
+  }, {
+    key: "componentWillMount",
+    value: function componentWillMount() {}
+  }, {
+    key: "componentDidMount",
+    value: function componentDidMount() {}
+  }, {
+    key: "componentDidShow",
+    value: function componentDidShow() {}
+  }, {
+    key: "componentDidHide",
+    value: function componentDidHide() {}
+  }, {
+    key: "componentDidCatchError",
+    value: function componentDidCatchError() {}
+    // 在 App 类中的 render() 函数没有实际作用
+    // 请勿修改此函数
+
+  }, {
+    key: "turnFn",
+    value: function turnFn(e) {
+      console.log(e.currentTarget.dataset.id);
+      var id = e.currentTarget.dataset.id;
+      this.setState({
+        id_: id
+      });
+    }
+  }, {
+    key: "gohome",
+    value: function gohome() {
+      // 
+      _index2.default.navigateBack({}).catch(function (error) {
+        console.log('既然没有上一页，我就返回首页了哦！');
+        _index2.default.navigateTo({
+          url: '/pages/index/index'
+        });
+      });
+    }
+  }, {
+    key: "_createData",
+    value: function _createData() {
+      var _this2 = this;
+
+      this.__state = arguments[0] || this.state || {};
+      this.__props = arguments[1] || this.props || {};
+      var __isRunloopRef = arguments[2];
+      ;
+
+      var loopArray0 = this.__state.server.map(function (item, index) {
+        item = {
+          $original: (0, _index.internal_get_original)(item)
+        };
+        var $loopState__temp2 = _this2.__state.id_ == Number(item.$original.id) ? 'box select' : 'box';
+        return {
+          $loopState__temp2: $loopState__temp2,
+          $original: item.$original
+        };
+      });
+
+      Object.assign(this.__state, {
+        loopArray0: loopArray0
+      });
+      return this.__state;
+    }
+  }]);
+
+  return Login;
+}(_index.Component), _class.properties = {}, _class.$$events = ["turnFn", "gohome"], _temp2);
+exports.default = Login;
+
+Component(require('../../npm/@tarojs/taro-weapp/index.js').default.createComponent(Login, true));
