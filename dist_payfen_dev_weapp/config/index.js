0c8cac0189eb4c00ee95a53c8f8ebb7b09385b0a
sutootototo2002 <sutootototo2002@163.com>
2019-07-04 18:38:00 +0800

    支付分正式版002

---
+++ dist_payfen_dev_weapp/config/index.js
@@ -0,0 +1,71 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.set = set;
+exports.get = get;
+
+
+/** 
+ * 线上环境
+ * 为了方便测试，使用的是聚合数据免费接口
+ *BASE_URL: 'https://t.wemall.com.cn/clientapi/',
+  HOST_URL: 'https://t.wemall.com.cn/',
+  HOST_WEBSOCKET: 'wss://t.wemall.com.cn/socket/',
+ */
+
+var BASE_URL = exports.BASE_URL = 'https://t.wemall.com.cn/clientapi/';
+
+/** 
+ * 测试环境
+ */
+var HOST_URL = exports.HOST_URL = 'https://t.wemall.com.cn/';
+
+/*
+ * WEBSOCKET
+ */
+var HOST_WEBSOCKET = exports.HOST_WEBSOCKET = 'wss://t.wemall.com.cn/socket/';
+/** 
+ * 线上mock
+ */
+var MOCKHOST = exports.MOCKHOST = 'http://xxx/mock';
+
+/** 
+ * 是否mock
+ */
+var ISMOCK = exports.ISMOCK = false;
+
+/**
+ * 当前的host  ONLINEHOST | QAHOST | MOCKHOST
+ */
+var MAINHOST = exports.MAINHOST = BASE_URL;
+
+/**
+ * 全局的分享信息 不用每一个都去写
+ */
+var SHAREINFO = exports.SHAREINFO = {
+  'title': '分享标题',
+  'path': '路径',
+  'imageUrl': '图片'
+};
+var PATH = exports.PATH = "https://shoptestapp.wemall.com.cn";
+var systemUser = exports.systemUser = '知码小蜜';
+var globalData = exports.globalData = {
+  token: '',
+  haslogin: false,
+  isshopping: 0,
+  needrefresh: false,
+  requestcount: 0,
+  timerTem: 0,
+  sysTitle: '知码开门售货柜',
+  intervalPapayTemp: null
+};
+
+function set(key, val) {
+  globalData[key] = val;
+}
+
+function get(key) {
+  return globalData[key];
+}
